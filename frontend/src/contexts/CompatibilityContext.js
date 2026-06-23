328d18f0f14b7778db9b15981509482d7d7a3f05
Pranav-Singh-Devloper <pranavsinghprimo@gmail.com>
2026-06-23 13:05:41 +0530

    fix: export restoreMatchSession in context provider

--- frontend/src/contexts/CompatibilityContext.js
+++ frontend/src/contexts/CompatibilityContext.js
@@ -552,6 +552,7 @@ export function CompatibilityProvider({ children }) {
       isChatOpen, setIsChatOpen,
       showCalculations, setShowCalculations,
       fetchRecentMatches,
+      restoreMatchSession,
       handleResetQuota,
       handleLogout,
       handleCompatibilityMatch
